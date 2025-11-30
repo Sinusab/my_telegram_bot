c15e7fe07d0bbce44c27c2571753312304309168
Sina Abyar <134528508+Sinusab@users.noreply.github.com>
2025-12-01 00:15:21 +0330

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-TELEGRAM_API_KEY = os.getenv("TELEGRAM_API_KEY")
-BRS_API_KEY = os.getenv("BRS_API_KEY")
-BRS_API_URL = "https://BrsApi.ir/Api/Market/Gold_Currency.php"
